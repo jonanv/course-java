b604a037cbf53da12ed114585a349e2dac9eadc0
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-08-21 22:38:17 -0500

    Se hizo programa de prueba declarando e iniciando programa con int

---
+++ Prueba.java
@@ -0,0 +1,6 @@
+public class Prueba {
+    public static void main(String[] args) {
+        int numero1 = 23;
+        System.out.println("El numero que se declaro y se incio fue: " + numero1);
+    }
+}
