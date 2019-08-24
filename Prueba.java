7646b026cf52a961b589b89715c74726bb0e4965
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-08-23 23:23:51 -0500

    Se agrego una variable, se inicializo y se declaro numero2

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,8 @@
 public class Prueba {
     public static void main(String[] args) {
-        byte numero1 = (byte) 23;
+        byte numero1 = (byte) 27;
+        short numero2 = (short) 30;
         System.out.println("El numero que se declaro y se incio fue: " + numero1);
+        System.out.println("Número 2: " + numero2);
     }
 }
