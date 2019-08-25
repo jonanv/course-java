0602b62d200454a2de1285e743ebf2b1e3cb8535
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-08-24 23:26:23 -0500

    Se agrego tipo de dato primitivo int

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,9 @@ public class Prueba {
     public static void main(String[] args) {
         byte numero1 = (byte) 27;
         short numero2 = (short) 30;
+        int numero3 = 2345;
         System.out.println("El numero que se declaro y se incio fue: " + numero1);
         System.out.println("Número 2: " + numero2);
+        System.out.println("Número 3: " + numero3);
     }
 }
