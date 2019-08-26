9599fb8fabe548d78cc6a8070882371ed3e510c1
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-08-25 21:41:39 -0500

    Hola mundo por variables

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde Java");
+        String holaMundo = "Hola mundo en Java";
+
+        System.out.println(holaMundo);
+        System.out.println(holaMundo);
+        System.out.println(holaMundo);
     }
 }
