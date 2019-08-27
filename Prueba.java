ede38471d617c00712753b6241c550e3d2ddf05a
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-08-26 21:02:56 -0500

    Trabajando con cadenas

--- Prueba.java
+++ Prueba.java
@@ -6,5 +6,13 @@ public class Prueba {
         System.out.println("El numero que se declaro y se inicio fue: " + numero1);
         System.out.println("Numero 2: " + numero2);
         System.out.println("Numero 3: " + numero3);
+
+        String nombre = "Giovanni";
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b" + nombre);
+        System.out.println("Retorno de carro: \r" + nombre);
+        System.out.println("Comilla simple: \'" + nombre + "\'");
     }
 }
