e28304dac679f5c420d4f82ae4f34b0a199783ab
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-08-26 20:50:20 -0500

    Se eliminaron los caracteres extraños para su compilación

--- Prueba.java
+++ Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
         short numero2 = (short) 30;
         int numero3 = 2345;
         System.out.println("El numero que se declaro y se inicio fue: " + numero1);
-        System.out.println("Número 2: " + numero2);
-        System.out.println("Número 3: " + numero3);
+        System.out.println("Numero 2: " + numero2);
+        System.out.println("Numero 3: " + numero3);
     }
 }
