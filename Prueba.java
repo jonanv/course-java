d13fe3c526ebbf42b65e2a8b1076bba4de82f548
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-09-05 19:27:39 -0500

    Tipos de datos primitivos

--- Prueba.java
+++ Prueba.java
@@ -29,13 +29,30 @@ public class Prueba {
         // String autor = scanner.nextLine();
         // System.out.println(libro + " fue escrito por " + autor);
 
-        byte b1 = 10;
-        System.out.println("Byte decimal: b1 = " + b1);
-        byte b2 = 0xa;
-        System.out.println("Byte hexadecimal: b2 = " + b2);
-        byte b3 = 012;
-        System.out.println("Byte ocatal: b3 = " + b3);
-        byte b4 = 0b1010;
-        System.out.println("Byte binario: b4 = " + b4);
+        // byte b1 = 10;
+        // System.out.println("Byte decimal: b1 = " + b1);
+        // byte b2 = 0xa;
+        // System.out.println("Byte hexadecimal: b2 = " + b2);
+        // byte b3 = 012;
+        // System.out.println("Byte ocatal: b3 = " + b3);
+        // byte b4 = 0b1010;
+        // System.out.println("Byte binario: b4 = " + b4);
+
+        System.out.println();
+        System.out.println("bits tipo int: " + Integer.SIZE);
+        System.out.println("bytes tipo int: " + Integer.BYTES);
+        System.out.println("Valor maximo tipo int: " + Integer.MAX_VALUE);
+        System.out.println("Valor minimo tipo int: " + Integer.MIN_VALUE);
+        // TODO: Hacer el resto de Wrapper para los tipos de datos primitivos reales.
+        System.out.println();
+        System.out.println("bits tipo float: " + Float.SIZE);
+        System.out.println("bytes tipo float: " + Float.BYTES);
+        System.out.println("Valor maximo tipo float: " + Float.MAX_VALUE);
+        System.out.println("Valor minimo tipo float: " + Float.MIN_VALUE);
+        System.out.println();
+        System.out.println("bits tipo double: " + Double.SIZE);
+        System.out.println("bytes tipo doublet: " + Double.BYTES);
+        System.out.println("Valor maximo tipo doublet: " + Double.MAX_VALUE);
+        System.out.println("Valor minimo tipo double: " + Double.MIN_VALUE);
     }
 }
