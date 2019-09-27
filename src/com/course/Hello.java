f7828ec0fd9daca976c542d4d9e39a64117d45d5
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-09-26 21:37:38 -0500

    Se creo un nuevo proyecto

--- src/com/course/Hello.java
+++
@@ -1,9 +0,0 @@
-class Hello {
-    public static void main(String[] args) {
-        String holaMundo = "Hola mundo en Java";
-
-        System.out.println(holaMundo);
-        System.out.println(holaMundo);
-        System.out.println(holaMundo);
-    }
-}
---
+++ src/com/course/Main.java
@@ -0,0 +1,8 @@
+package com.course;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Curso de Java con IntelliJ IDEA");
+    }
+}
