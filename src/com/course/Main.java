9a97d0c2b3d800b6fbfc183e22278d3ebb2ea2fa
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-09-28 15:03:57 -0500

    Caracteres especiales en Java

--- src/com/course/Main.java
+++ src/com/course/Main.java
@@ -5,10 +5,17 @@ public class Main {
     public static void main(String[] args) {
 
         String saludar = "Saludos desde Java en IntelliJ IDEA";
-        System.out.println(saludar);
-        System.out.println(saludar);
-        System.out.println(saludar);
+        String nombre = "Giovanni";
+        String apellido = "Vargas";
 
-
+        System.out.println(saludar);
+        System.out.println(nombre + " " + apellido);
+        System.out.println("Nueva línea :  \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b" +  nombre);
+        System.out.println("Retorno de carro: \r" + nombre);
+        System.out.println("Comilla simple: \'" + nombre + "\'");
+        System.out.println("Comilla simple: '" + nombre + "'");
+        System.out.println("Comilla double: \"" + nombre + "\"");
     }
 }
