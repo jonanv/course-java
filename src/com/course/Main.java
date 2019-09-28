6197a2d54234c0a64d1978fbc3f39580a0f97f68
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-09-28 15:43:23 -0500

    valores maximos de los tipos primitivos byte, short, int, long y el literal de long L

--- src/com/course/Main.java
+++ src/com/course/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String saludar = "Saludos desde Java en IntelliJ IDEA";
+        /*String saludar = "Saludos desde Java en IntelliJ IDEA";
         String nombre = "Giovanni";
         String apellido = "Vargas";
 
@@ -27,6 +27,38 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el nombre de usuario: ");
         String usuario = scanner.nextLine();
-        System.out.println(usuario);
+        System.out.println(usuario);*/
+
+        byte byteVar = 127;
+        System.out.println("byte: " + byteVar);
+        System.out.println("bits tipo byte: " + Byte.SIZE);
+        System.out.println("bytes tipo byte: " + Byte.BYTES);
+        System.out.println("valor mínimo tipo byte: " + Byte.MIN_VALUE);
+        System.out.println("valor máximo tipo byte: " + Byte.MAX_VALUE);
+        System.out.println();
+
+        short shorVar = 32767;
+        System.out.println("short: " + shorVar);
+        System.out.println("bits tipo short: " + Short.SIZE);
+        System.out.println("bytes tipo short: " + Short.BYTES);
+        System.out.println("valor mínimo tipo short: " + Short.MIN_VALUE);
+        System.out.println("valor máximo tipo short: " + Short.MAX_VALUE);
+        System.out.println();
+
+        int intVar = 33333333;
+        System.out.println("int: " + intVar);
+        System.out.println("bits tipo int: " + Integer.SIZE);
+        System.out.println("bytes tipo int: " + Integer.BYTES);
+        System.out.println("valor mínimo tipo int: " + Integer.MIN_VALUE);
+        System.out.println("valor máximo tipo int: " + Integer.MAX_VALUE);
+        System.out.println();
+
+        long longVar = 9223372036854775807L;
+        System.out.println("long: " + longVar);
+        System.out.println("bits tipo long: " + Long.SIZE);
+        System.out.println("bytes tipo long: " + Long.BYTES);
+        System.out.println("valor mínimo tipo long: " + Long.MIN_VALUE);
+        System.out.println("valor máximo tipo long: " + Long.MAX_VALUE);
+        System.out.println();
     }
 }
