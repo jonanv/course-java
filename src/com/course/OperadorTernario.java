fac4c0a9cb6885f6c10b69edf8ed92d26333ed50
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-09-29 16:44:25 -0500

    Se hizo la clase OperadorTernario

---
+++ src/com/course/OperadorTernario.java
@@ -0,0 +1,19 @@
+package com.course;
+
+import java.util.Scanner;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero1 = 0;
+        int numero2 = 0;
+
+        System.out.println("Proporciona el número 1: ");
+        numero1 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Proporciona el número 2: ");
+        numero2 = Integer.parseInt(scanner.nextLine());
+
+        int numeroMayor = (numero1 > numero2) ? numero1 : numero2;
+        System.out.println("El número mayor es = " + numeroMayor);
+    }
+}
