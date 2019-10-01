6c4407b5d69575e2ae3c353a5af25d98da97a2c2
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-01 07:55:12 -0500

    Flujo de control con if

---
+++ src/com/course/ControlDeFlujo.java
@@ -0,0 +1,14 @@
+package com.course;
+
+public class ControlDeFlujo {
+    public static void main(String[] args) {
+        boolean condicion = false;
+
+        if (condicion){
+            System.out.println("Condición verdadera");
+        }
+        else {
+            System.out.println("Condición falsa");
+        }
+    }
+}
