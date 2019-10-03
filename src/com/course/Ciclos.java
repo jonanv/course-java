f88d5a0f9d420235cf188120d98b55aaffe67e8a
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-03 14:22:14 -0500

    Se hace ejemplo ciclo do-while

---
+++ src/com/course/Ciclos.java
@@ -0,0 +1,11 @@
+package com.course;
+
+public class Ciclos {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Imprimiendo numero: " + i);
+            i++;
+        }while (i < 10);
+    }
+}
