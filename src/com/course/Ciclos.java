15095028bf6dd06af03bb42990f397cf6b06ba66
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-04 07:50:04 -0500

    Sse hizo ciclo while

--- src/com/course/Ciclos.java
+++ src/com/course/Ciclos.java
@@ -7,6 +7,14 @@ public class Ciclos {
             System.out.println("Imprimiendo número do-while: " + i);
             i++;
         }while (i < 10);
+        System.out.println();
+
+        i = 0;
+        while (i < 10){
+            System.out.println("Imprimiendo número while: " + i);
+            i++;
+        }
+        System.out.println();
 
         for (int j = 0; j < 10; j++) {
             System.out.println("Imprimiendo número for: " + j);
