9efbe573213a56af35f001f5f909c1900822a2c3
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-04 08:57:59 -0500

    Se hizo break y continue en ciclos

--- src/com/course/Ciclos.java
+++ src/com/course/Ciclos.java
@@ -19,5 +19,21 @@ public class Ciclos {
         for (int j = 0; j < 10; j++) {
             System.out.println("Imprimiendo número for: " + j);
         }
+        System.out.println();
+
+        for (int j = 0; j < 10; j++) {
+            if (j % 2 == 0) {
+                System.out.println("Imprimiendo número for: " + j);
+                break;
+            }
+        }
+        System.out.println();
+
+        for (int j = 0; j < 10; j++) {
+            if (j % 2 != 0) {
+                continue;
+            }
+            System.out.println("Imprimiendo número for: " + j);
+        }
     }
 }
