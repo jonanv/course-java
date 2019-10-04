7f4da7b531e9c1c4adfe723f76e23f56d7dd4d6f
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-04 07:46:06 -0500

    Se hace ciclo for

--- src/com/course/Ciclos.java
+++ src/com/course/Ciclos.java
@@ -4,8 +4,12 @@ public class Ciclos {
     public static void main(String[] args) {
         int i = 0;
         do {
-            System.out.println("Imprimiendo numero: " + i);
+            System.out.println("Imprimiendo número do-while: " + i);
             i++;
         }while (i < 10);
+
+        for (int j = 0; j < 10; j++) {
+            System.out.println("Imprimiendo número for: " + j);
+        }
     }
 }
